b58431e102a2c58c19964cd5aae11934df0c3e47
GuilhermeAlves <alvesmigliorini@hotmail.com>
2022-04-26 19:20:01 -0300

    layout updated

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,14 +1,24 @@
 import styles from '../styles/Banner.module.css'
 import Image from 'next/image'
 import logoImage from '../assets/images/logo_gfit.png'
+import fundoImage from '../assets/images/fundo.png'
 
 function Banner() {
   return (
-    <div className={styles.container}>
-      <div className={styles.container_image}>
-        <Image
-        src={logoImage}
-        />
+    <div className={styles.banner}>
+      <div className={styles.banner_fundo_image}>
+        
+      </div>
+
+      <div className={styles.banner_logo_image}>
+        <Image src={logoImage}/>
+      </div>
+
+      <div className={styles.banner_textbox}>
+        <h1 className={styles.banner_textbox_title}>Info</h1>
+        <p className={styles.banner_textbox_text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Placeat voluptatibus dolores autem veritatis voluptatum impedit, blanditiis quo dignissimos earum
+         at quas iure facere sunt laboriosam ab nisi amet aliquid ipsum!.</p>
       </div>
     </div>
   )
