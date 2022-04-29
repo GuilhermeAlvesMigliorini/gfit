71878961e15a572efc9f14a26d3584a0014914c0
GuilhermeAlves <alvesmigliorini@hotmail.com>
2022-04-29 11:58:18 -0300

    layout updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,8 +5,6 @@ import styles from '../styles/Footer.module.css'
 function Footer() {
   return (
     <div className={styles.footer}>
-      <div className={styles.footer_image}></div>
-      
     </div>
   )
 }
