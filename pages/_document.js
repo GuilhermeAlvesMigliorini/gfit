fd23e072980d6538bbbc9a4dd300f1cc7174bc68
GuilhermeAlves <alvesmigliorini@hotmail.com>
2022-05-02 11:51:26 -0300

    layout updated

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>GFit</title>
+          <meta http-equiv="content-language" content="pt-br" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400&display=swap" rel="stylesheet" />
