b91bc007e944007cf6612d4d6569f6045032f543
GuilhermeAlves <alvesmigliorini@hotmail.com>
2022-05-02 10:48:18 -0300

    layout updated

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400&display=swap" rel="stylesheet" />
         </Head>
         <body>
