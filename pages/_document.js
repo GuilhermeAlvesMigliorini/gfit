904fdb62442e71cdb388a53ce93dd62a8018ac61
GuilhermeAlves <alvesmigliorini@hotmail.com>
2022-05-02 12:13:24 -0300

    layout updated

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title GFit />
+          <title>GFit</title>
           <meta httpEquiv="content-language" content="pt-br" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
