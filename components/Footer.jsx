a384e6739e5bf7eb3958494b1f7dad49272f9925
GuilhermeAlves <alvesmigliorini@hotmail.com>
2022-05-03 10:40:31 -0300

    layout updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,13 +7,13 @@ function Footer() {
     <div className={styles.footer}>
       <div className={styles.footer_social} >
         <a href='https://www.instagram.com' className={styles.footer_social_icon} >
-        <i class="fa-brands fa-instagram"></i>
+        <i className="fa-brands fa-instagram"></i>
         </a>
         <a href='https://www.facebook.com' className={styles.footer_social_icon} >
-        <i class="fa-brands fa-facebook"></i>
+        <i className="fa-brands fa-facebook"></i>
         </a>
         <a href='https://www.whatsapp.com' className={styles.footer_social_icon} >
-        <i class="fa-brands fa-whatsapp"></i>
+        <i className="fa-brands fa-whatsapp"></i>
         </a>
         
       </div>
